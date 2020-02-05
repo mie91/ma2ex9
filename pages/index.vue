df75a5f0d51f112ca3167e37e61b33a3781e6490
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-02-05 20:19:32 +0100

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,6 @@
 <template>
   <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        ma2ex9
-      </h1>
-      <h2 class="subtitle">
-        exercise for ma 2 workflow 2
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+    <h1>Marie Helen Berger</h1>
   </div>
 </template>
 
